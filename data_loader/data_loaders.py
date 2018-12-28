85c359d45312c54291ce120a5d7fec8394bc074b
Nishanth Koganti <buntyke@gmail.com>
2018-12-28 17:21:45 +0900

    add lstm dataloader and policy

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -33,14 +33,79 @@ class MujocoDataset(Dataset):
         # return dataset length
         return self.data_len
 
+class MujocoSeqDataset(Dataset):
+    """
+    Dataset class for mujoco expert datasets
+    """
+    def __init__(self, pickle_file, seq_size=30):
+        """
+        Args:
+            pickle_file (string): path to dataset pickle file
+        """
+        # set sequence size
+        self.seq_size = seq_size
+
+        # load the pickle file
+        with open(pickle_file, 'rb') as f:
+            dataset = pickle.load(f)
+
+        # get actions and observations
+        seq_tails = np.where(dataset['dones'])[0]+1
+        observations = dataset['observations'].astype(np.float32)
+        actions = np.squeeze(dataset['actions']).astype(np.float32)
+
+        actions = np.split(actions, seq_tails)[:-1]
+        observations = np.split(observations, seq_tails)[:-1]
+
+        # split into small sequences
+        for n in range(len(actions)):
+            act_array = actions[n]
+            obs_array = observations[n]
+            n_seqs = int(act_array.shape[0]/self.seq_size)
+            traj_len = act_array.shape[0]-act_array.shape[0]%self.seq_size
+
+            act_array = np.split(act_array[:traj_len], n_seqs)
+            obs_array = np.split(obs_array[:traj_len], n_seqs)
+            if act_array[-1].shape[0] < self.seq_size:
+                act_array = act_array[:-1]
+                obs_array = obs_array[:-1]
+
+            actions[n] = act_array
+            observations[n] = obs_array
+
+        # assign class variables
+        self.act_seqs = np.concatenate(actions, axis=0)
+        self.obs_seqs = np.concatenate(observations, axis=0)
+
+        # calculate len
+        self.data_len = self.act_seqs.shape[0]
+
+    def __getitem__(self, index):
+        # return state, action pair
+        return (self.obs_seqs[index,:], self.act_seqs[index,:])
+
+    def __len__(self):
+        # return dataset length
+        return self.data_len
+
 class MujocoDataLoader(BaseDataLoader):
     """
     Data loader for mujoco expert datasets
     """
     def __init__(self, pickle_file, batch_size, shuffle, 
                  validation_split, num_workers):
-        self.pickle_file = pickle_file
-        self.dataset = MujocoDataset(self.pickle_file)
+        self.dataset = MujocoDataset(pickle_file)
 
-        super(MujocoDataLoader, self).__init__(self.dataset, batch_size, shuffle,
-                                               validation_split, num_workers)
+        super(MujocoDataLoader, self).__init__(self.dataset, batch_size, 
+                                shuffle, validation_split, num_workers)
+
+class MujocoSeqDataLoader(BaseDataLoader):
+    """
+    Data loader for mujoco expert datasets
+    """
+    def __init__(self, pickle_file, seq_size, batch_size, shuffle, 
+                 validation_split, num_workers):
+        self.dataset = MujocoSeqDataset(pickle_file, seq_size)
+
+        super(MujocoSeqDataLoader, self).__init__(self.dataset, batch_size, 
+                                    shuffle, validation_split, num_workers)
--- model/model.py
+++ model/model.py
@@ -1,6 +1,9 @@
+import torch
 import torch.nn as nn
-from base import BaseModel
 import torch.nn.functional as F
+import torch.autograd as autograd
+
+from base import BaseModel
 
 
 class MujocoPolicy(BaseModel):
@@ -21,4 +24,43 @@ class MujocoPolicy(BaseModel):
         act = F.relu(self.fc1(obs))
         act = F.relu(self.fc2(act))
         act = self.fc3(act)
+        return act
+
+class MujocoLstmPolicy(BaseModel):
+    """
+    LSTM Policy used for Mujoco environments
+    2 fully connected layers, lstm layer and cont. output
+    """
+    def __init__(self, obs_dim, act_dim, batch_size, 
+                 hidden_dim=256, num_layers=1):
+        super(MujocoLstmPolicy, self).__init__()
+
+        # assign class variables
+        self.obs_dim = obs_dim
+        self.act_dim = act_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.batch_size = batch_size
+
+        # initialize 3 layers
+        self.fc1 = nn.Linear(self.obs_dim, self.hidden_dim)
+        self.lstm = nn.LSTM(self.hidden_dim, self.hidden_dim, 
+                            self.num_layers, batch_first=True)
+        self.fc2 = nn.Linear(self.hidden_dim, self.act_dim)
+
+        # initialize hidden variable
+        self.hidden = self.init_hidden()
+
+    def init_hidden(self):
+        hx = autograd.Variable(torch.zeros(self.num_layers, 
+                               self.batch_size, self.hidden_dim).cuda())
+        cx = autograd.Variable(torch.zeros(self.num_layers,
+                               self.batch_size, self.hidden_dim).cuda())
+        return (hx,cx)
+
+    def forward(self, obs):
+        # perform forward computation
+        hidden = F.relu(self.fc1(obs))
+        act, self.hidden = self.lstm(hidden, self.hidden)
+        act = self.fc2(act)
         return act
